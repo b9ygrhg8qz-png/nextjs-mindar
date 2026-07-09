f6760e21407fdf8dd887d87fa7b57c5ed4de0166
b9ygrhg8qz-png <b9ygrhg8qz@privaterelay.appleid.com>
2026-07-09 14:19:20 +0500

    Fix: Remove React reference error in _app.js and viewport warning in _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  // Load MindAR script in head
-  React.useEffect(() => {
-    if (typeof window !== 'undefined') {
-      // Script will be loaded dynamically by ARViewer component
-    }
-  }, [])
-
   return <Component {...pageProps} />
 }
